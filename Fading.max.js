cf2de5b4c5cf514ce2be463a9827b7561a2ce7a4
Moderator <reusablecondom@email.com>
2018-10-22 21:18:22 +0100

    0.0.7

---
+++ Fading.max.js
@@ -0,0 +1,111 @@
+﻿// Animation: Fade In
+// Varient(s): Normal
+(function() {
+
+    // Define Element
+    var element = document.getElementsByClassName("something")[0];
+
+    // Show Element
+    element.style.visibility = "visible";
+
+    // Grab Opacity
+    var opacity = parseFloat(window.getComputedStyle(element, null).getPropertyValue("opacity"));
+
+    // Animation
+    (function () { var fadeIn = setInterval(function() {
+        if (opacity >= 1.0) {
+            clearInterval(fadeIn);
+            element.style.opacity = "1.0";
+        } else {
+            opacity = (opacity + 0.01);
+            element.style.opacity = opacity;
+        }
+    }, 10) })();
+
+})();
+
+// Animation: Fade In
+// Varient(s): Progressive
+(function() {
+
+    // Define Element
+    var element = document.getElementsByClassName("something")[0];
+
+    // Show Element
+    element.style.visibility = "visible";
+
+    // Grab Opacity
+    var opacity = parseFloat(window.getComputedStyle(element, null).getPropertyValue("opacity"));
+
+    // Animation
+    (function () { var fadeIn = setInterval(function() {
+        if (opacity >= 1.0) {
+            clearInterval(fadeIn);
+            element.style.opacity = "1.0";
+        } else if (opacity > 0.6 && opacity < 1.0) {
+            opacity = (opacity + 0.008);
+            element.style.opacity = opacity;
+        } else if (opacity > 0.3 && opacity < 0.6) {
+            opacity = (opacity + 0.005);
+            element.style.opacity = opacity;
+        } else if (opacity < 0.3) {
+            opacity = (opacity + 0.002);
+            element.style.opacity = opacity;
+        }
+    }, 10) })();
+
+})();
+
+// Animation: Fade Out
+// Varient(s): Normal
+(function() {
+
+    // Define Element
+    var element = document.getElementsByClassName("something")[0];
+
+    // Grab Opacity
+    var opacity = parseFloat(window.getComputedStyle(element, null).getPropertyValue("opacity"));
+
+    // Animation
+    (function () { var fadeOut = setInterval(function() {
+        if (opacity <= 0.0) {
+            clearInterval(fadeOut);
+            element.style.opacity = "0.0";
+            element.style.visibility = "hidden";
+        } else {
+            opacity = (opacity - 0.01);
+            element.style.opacity = opacity;
+        }
+    }, 10) })();
+
+})();
+
+// Animation: Fade In
+// Varient(s): Remove
+(function() {
+
+    // Define Element
+    var element = document.getElementsByClassName("something")[0];
+
+    // Grab Opacity
+    var opacity = parseFloat(window.getComputedStyle(element, null).getPropertyValue("opacity"));
+
+    // Animation
+    (function () { var fadeOut = setInterval(function() {
+        if (opacity <= 0.0) {
+            clearInterval(fadeOut);
+            element.style.opacity = "0.0";
+            element.style.visibility = "hidden";
+
+            // Remove Element
+            setTimeout(function() {
+                element.remove();
+            }, 250);
+
+        } else {
+            opacity = (opacity - 0.01);
+            element.style.opacity = opacity;
+        }
+    }, 10) })();
+
+})();
--- Ripple.max.js
+++ Ripple.max.js
@@ -1,30 +1,30 @@
 ﻿document.addEventListener("DOMContentLoaded", function(event) {
 
-    var links = document.querySelectorAll('.ripple');
-    
-    for (var i = 0, len = links.length; i < len; i++) {
-    
-        links[i].addEventListener('click', function(e) {
-    
+    var selected = document.querySelectorAll('.ripple');
+
+    for (var i = 0, len = selected.length; i < len; i++) {
+
+        selected[i].addEventListener('click', function(e) {
+
             var targetElement = e.target;
             var inkElement = targetElement.querySelector('.ripple-overlay');
-    
+
             if (inkElement) {
                 inkElement.classList.remove('ripple-effect');
-    
+
             } else {
                 inkElement = document.createElement('div');
                 inkElement.classList.add('ripple-overlay');
                 inkElement.style.width = inkElement.style.height = Math.max(targetElement.offsetWidth, targetElement.offsetHeight) + 'px';
                 targetElement.appendChild(inkElement);
             }
-    
+
             inkElement.style.left = (e.offsetX - inkElement.offsetWidth / 2) + 'px';
             inkElement.style.top = (e.offsetY - inkElement.offsetHeight / 2) + 'px';
             inkElement.classList.add('ripple-effect');
-    
+
         }, false);
-    
+
     }
 
 });
